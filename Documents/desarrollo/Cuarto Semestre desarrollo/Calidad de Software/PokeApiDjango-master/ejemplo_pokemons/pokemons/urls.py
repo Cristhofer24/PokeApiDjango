2bbc9682e59464fecf348d668688a2a51c9a85d3
daya18m <dannacorrea665@gmail.com>
2025-03-08 08:32:07 -0500

    estilos y paginacion

---
+++ Documents/desarrollo/Cuarto Semestre desarrollo/Calidad de Software/PokeApiDjango-master/ejemplo_pokemons/pokemons/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import pokemon_list,pokemon_detail
+
+
+urlpatterns = [
+    path("", pokemon_list, name="pokemon_list"),
+    path("pokemon/<int:id>", pokemon_detail, name='pokemon_detail'),
+   
+]
